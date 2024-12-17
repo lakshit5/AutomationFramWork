4b3ad8eec9f5c60338828a7e17ae240d1878e874
lakshit sharma <lakshitsharma2255@gmail.com>
2024-12-17 12:25:14 +0530

    pushCommit1

---
+++ zz.com.Demo2/src/test/java/zz/com/Demo2/Day1.java
@@ -0,0 +1,36 @@
+package zz.com.Demo2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class Day1 {
+
+	//WebDriver driver =new ChromeDriver();
+	//@Test
+	@Test
+	public void Day1() {
+		WebDriver driver;
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	
+ 	
+	 
+		if (BROWSER.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+			
+		} else if (BROWSER.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (BROWSER.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			driver = new ChromeDriver();
+		}	
+		driver.get("https://www.flipkart.com/");
+		System.out.println("Browser launch sucessfully ");
+	}
+}
+	
